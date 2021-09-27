9d097ebf1e3e5ee1a899cc724aa644f7b477f3b1
QuangPham <quang20201@gmail.com>
2021-09-27 21:23:35 +0700

    add creating data feature

--- myapp/index.js
+++ myapp/index.js
@@ -12,6 +12,13 @@ const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
+const _data = require('./lib/data');
+
+// testing 
+// @TODO delete this
+_data.create('test', 'newFile', { 'foo': 'bar' }, function (err) {
+    console.log('this was the error', err);
+})
 
 // instantiate the HTTP server
 const httpServer = http.createServer(function (req, res) {
---
+++ myapp/lib/data.js
@@ -0,0 +1,50 @@
+/**
+ * Library for storing data and editing data
+ * 
+ */
+
+// dependencies
+const fs = require('fs');
+const path = require('path');
+
+// container for the module (to be exported)
+const lib = {};
+
+// base the directory of the .data folder
+lib.baseDir = path.join(__dirname, '/../.data/');
+
+// write data to file
+lib.create = function (dir, file, data, callback) {
+    // open the file for writing
+    const fileToWrite = lib.baseDir + dir + '/' + file + '.json';
+    fs.open(fileToWrite, 'wx', function (err, fileDescriptor) {
+        if (!err && fileDescriptor) {
+            // convert data to string
+            const stringData = JSON.stringify(data);
+
+            // write data to file
+            fs.writeFile(fileDescriptor, stringData, function (err) {
+                if (!err) {
+                    fs.close(fileDescriptor, function (err) {
+                        if (!err) {
+                            callback(false);
+                        } else {
+                            callback('Error closing new file');
+                        }
+                    })
+                } else {
+                    callback('Error writing to new file');
+                }
+            });
+
+
+        } else {
+            callback('Could not create new file, it may already existed');
+        }
+    });
+};
+
+
+
+// export the module
+module.exports = lib;
